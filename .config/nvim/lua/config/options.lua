9406326cb96728c2f716189c5195b481b4ee71f2
David Yang <dyang@justworks.com>
2024-07-18 13:18:37 -0400

    nvim configs

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,26 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.diagnostic.enable(false)
+
+-- Basic settings
+vim.opt.number = true
+-- vim.opt.relativenumber = true  -- This line is commented out like in the Vimscript
+
+-- Syntax on
+vim.cmd("syntax on")
+
+-- Incremental search
+vim.opt.incsearch = true
+
+-- Highlight search results
+vim.opt.hlsearch = true
+
+-- Use the unnamed register for the clipboard
+vim.opt.clipboard = "unnamed"
+
+-- Ignore case in search patterns
+vim.opt.ignorecase = true
+
+-- Override ignorecase if search pattern contains uppercase letters
+vim.opt.smartcase = true
